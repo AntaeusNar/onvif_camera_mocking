8868d88b1df73121e8f036e089ab9398d56fd4b5
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-14 19:16:12 -0700

    Spacing

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -17,7 +17,7 @@ class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
 
     def do_create_element(self, url):
         global color
-        mock_pipeline = "videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color={0} ! x264enc ! queue ! rtph264pay name=pay0 config-interval=1 pt=96".format(color) 
+        mock_pipeline = "videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color={0} ! x264enc ! queue ! rtph264pay name=pay0 config-interval=1 pt=96".format(color)
         print ("Pipeling launching: " + mock_pipeline)
         return Gst.parse_launch(mock_pipeline)
 
