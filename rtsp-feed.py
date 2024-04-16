7736f78f7723fedc877193de0e1b24e5095bbd6d
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-15 17:12:40 -0700

    test with visual

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -19,7 +19,7 @@ class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
 
     def do_create_element(self, url):
         global color
-        mock_pipeline = "audiotestsrc ! audioconvert ! autoaudiosink"
+        mock_pipeline = "audiotestsrc wave=2 freq=200 ! tee name=t ! queue ! audioconvert !  autoaudiosink t. ! queue ! audioconvert ! libvisual_lv_scope ! videoconvert ! autovideosink"
         # mock_pipeline = "videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color={0} ! x264enc ! queue ! rtph264pay name=pay0 config-interval=1 pt=96".format(color)
         print ("Pipeling launching: " + mock_pipeline)
         return Gst.parse_launch(mock_pipeline)
