d1319db637dee4db091673b0e124f67ab6606912
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-20 14:39:45 -0700

    changes

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -15,15 +15,15 @@ class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
 
     def do_create_element(self, url):
         audio_src = 'audiotestsrc wave=ticks apply-tick-ramp=true tick-interval=100000000 freq=261.63 volume=0.4 marker-tick-period=10 sine-periods-per-tick=20'
-        audio_enc = 'alawenc mux.'
-
+        audio_enc = ' ! alawenc'
 
         video_src = 'videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color=4080751'
-        video_enc = 'x264enc mux.'
+        video_enc = ' ! x264enc'
 
-        mux = 'mpegtsmux name=mux'
-        rtsp_payload = 'rtpmp2tpay pt=96 name=pay0'
-        pipeline_description = "{0} ! {1} ! {2} ! {3} ! {4} ! {5}".format(audio_src, audio_enc, video_src, video_enc, mux, rtsp_payload)
+        mux = ' ! mpegtsmux name=mux'
+        rtsp_payload = ' ! rtpmp2tpay pt=96 name=pay0'
+
+        pipeline_description = audio_src + audio_enc + video_src + video_enc + mux + rtsp_payload
 
         print("Launching Pipeline: " + pipeline_description)
         return Gst.parse_launch(pipeline_description)
