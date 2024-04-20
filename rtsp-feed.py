09540cee1c0b5399311247576198324fd840a2b4
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-20 16:33:42 -0700

    added !

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -35,7 +35,7 @@ class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
         elif test == 'video':
             pipeline_description = f"{video_pipeline} {video_rtsp}"
         elif test == 'mux':
-            pipeline_description = f"{audio_pipeline} ! aacparse ! queue {video_pipeline} ! h264parse ! queue {mux} ! {mux_rtsp}"
+            pipeline_description = f"{audio_pipeline} ! aacparse ! queue ! {video_pipeline} ! h264parse ! queue ! {mux} ! {mux_rtsp}"
         else:
             print("No pipeline selected. Exiting")
             exit(1)
