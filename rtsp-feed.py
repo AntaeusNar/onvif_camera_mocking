7e6a1fdbe19760cfa5db13d0887e4617851944da
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-20 11:45:46 -0700

    testing

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -19,7 +19,11 @@ class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
 
     def do_create_element(self, url):
         global color
-        mock_pipeline = "audiotestsrc wave=7 ! tee name=t ! wavescope ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 t.! alawenc ! rtppcmapay  name=pay1 pt=97"
+        mock_pipeline = """
+    audiotestsrc wave=7 ! tee name=t
+    t. ! queue ! wavescope shader=classic ! videoconvert ! x264enc ! video/x-h264,stream-format=byte-stream ! h264parse ! rtph264pay name=videopay
+    t. ! queue ! audioconvert ! voaacenc ! aacparse ! rtpmp4apay name=audiopay
+"""
         # mock_pipeline = "videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color={0} ! x264enc  ! rtph264pay name=pay0 pt=96 audiotestsrc is-live=0 ! audioconvert ! audio/x-raw,rate=(int)8000,channels=(int)1 ! alawenc ! rtppcmapay pt=97 name=pay1".format(color)
         # mock_pipeline = "videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color={0} ! x264enc ! queue ! rtph264pay name=pay0 config-interval=1 pt=96".format(color)
 
