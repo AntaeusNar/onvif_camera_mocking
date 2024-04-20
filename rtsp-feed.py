8e3d18648a435a3025567899e6b59246e2d6ec60
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-20 14:43:30 -0700

    more edits

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -15,15 +15,18 @@ class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
 
     def do_create_element(self, url):
         audio_src = 'audiotestsrc wave=ticks apply-tick-ramp=true tick-interval=100000000 freq=261.63 volume=0.4 marker-tick-period=10 sine-periods-per-tick=20'
-        audio_enc = ' ! alawenc'
+        audio_enc = ' ! audioconvert ! audioresample ! alawenc'
 
-        video_src = ' ! videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color=4080751'
-        video_enc = ' ! x264enc'
+        video_src = 'videotestsrc pattern=bar horizontal-speed=2 background-color=9228238 foreground-color=4080751'
+        video_enc = ' ! videoconvert ! x264enc'
 
-        mux = ' ! mpegtsmux name=mux'
-        rtsp_payload = ' ! rtpmp2tpay pt=96 name=pay0'
+        audio_pipeline = audio_src + audio_enc
+        video_pipeline = video_src + video_enc
 
-        pipeline_description = audio_src + audio_enc + video_src + video_enc + mux + rtsp_payload
+        mux = 'mpegtsmux name=mux'
+        rtsp_payload = 'rtpmp2tpay pt=96 name=pay0'
+
+        pipeline_description = f"{audio_pipeline} ! {video_pipeline} ! {mux} ! {rtsp_payload}"
 
         print("Launching Pipeline: " + pipeline_description)
         return Gst.parse_launch(pipeline_description)
