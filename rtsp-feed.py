a6b2a27c713f656997598d1a781c1665cb3bf766
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-23 19:13:20 -0700

    Switch back to normal rtsp

--- rtsp-feed.py
+++ rtsp-feed.py
@@ -114,6 +114,6 @@ if __name__ == '__main__':
     loop = GLib.MainLoop()
     Gst.init(None)
 
-    # s = GstreamerRtspServer()
-    s = GstreamerOnvifRtspServer()
+    s = GstreamerRtspServer()
+    # s = GstreamerOnvifRtspServer()
     loop.run()
