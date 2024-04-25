618a97412aa04626929ea4dfaac1fcba0b4895f8
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-04-24 18:50:31 -0700

    adding in the GObject

--- rtsp_feed.py
+++ rtsp_feed.py
@@ -6,7 +6,7 @@ import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
 
-from gi.repository import Gst, GstRtspServer, GLib
+from gi.repository import Gst, GstRtspServer, GLib, GObject
 
 # Creates the MediaFactory
 class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
