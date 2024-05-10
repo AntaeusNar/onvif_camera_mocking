107c75a1ea42dc4a4256b60449e2552ddda08ec3
AntaeusNar <Jedidiah.h.Wallace@gmail.com>
2024-05-09 19:15:27 -0700

    debug test

--- rtsp_feed.py
+++ rtsp_feed.py
@@ -131,6 +131,6 @@ class GstreamerOnvifRtspServer():
 # main Function
 if __name__ == '__main__':
 
-    s = GstreamerRtspServer()
-    # s = GstreamerOnvifRtspServer()
+    # s = GstreamerRtspServer()
+    s = GstreamerOnvifRtspServer()
     loop.run()
